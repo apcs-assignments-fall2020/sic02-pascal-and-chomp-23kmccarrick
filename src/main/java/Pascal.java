6307cfd2e0e734335f4dedd951b765583ed4a5ee
23kmccarrick <23kmccarrick@athenian.org>
2021-01-19 20:26:22 +0000

    bluh

--- src/main/java/Pascal.java
+++ src/main/java/Pascal.java
@@ -7,7 +7,14 @@
 public class Pascal
 {
     public static int[][] pascalTriangle(int n) {
-        // YOUR CODE HERE
-        return null;
+        int[][] bluh = new int[n][n];
+        bluh[0][0]=1;
+        for(int i = 0; i <bluh.length; i ++){
+            bluh[i][0]=1;
+            for(int j = 0;j < bluh[0].length; j++){
+                bluh[i][j]=bluh[i-1][j-1]+bluh[i-1][j];
+            }
+        }
+        return bluh;
     }
 }
